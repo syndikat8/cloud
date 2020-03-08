9a9581ff801fd36cb4c1d7cdb0d878ef7b204ef9
Dima Matusik <k.syndikat@gmail.com>
2020-03-08 21:16:37 +0300

    add

---
+++ src/components/MyName/MyName.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './MyName.module.css';
+
+
+
+const MyName = () => {
+  return  (
+    <div className={styles.myName}>
+      <h3>Dima matusik</h3>
+    </div>
+  )
+};
+
+export default MyName;
---
+++ src/components/message/Message.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Message.module.css';
+
+
+
+const Message = () => {
+  return  (
+    <div className={styles.message}>
+      <h6>Dima Matusik</h6>
+       elit. Impedit molestiae quam quo unde voluptate! Autem consequatur deserunt ducimus eaque, libero minima natus? Deserunt, doloremque ea et exercitationem nesciunt numquam vitae?
+ <div className={styles.time}>
+   04:19 PM
+ </div>
+    </div>
+  )
+};
+
+export default Message;
