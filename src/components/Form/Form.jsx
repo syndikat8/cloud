c99ec884779bffd225bfb19b768e5b9e9eab5ba6
Dima Matusik <k.syndikat@gmail.com>
2020-03-22 20:57:53 +0300

    add clickhandler

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,23 +2,36 @@ import React from 'react';
 import styles from './Form.module.css';
 
 
-const Form = () => {
+class Form  extends  React.Component{
 
-  let newMessageText = React.createRef();
 
-  let addMessag = () => {
-   let text = newMessageText.current.value;
-    alert("Привет "+ text);
-    newMessageText.current.value = "";
-  };
+  newMessageText = React.createRef()
 
-  return (
-    <div className={styles.form}>
-      <span>10000000</span>
-      <input ref={newMessageText} type="text"/>
-      <button onClick={addMessag}>send</button>
-    </div>
-  )
+  state = {
+    spanNumber: 10,
+  }
+
+
+  addClickHandler = () => {
+    let text = this.newMessageText.current.value
+    this.newMessageText.current.value = ""
+    alert("Привет " + text)
+  this.setState({
+    spanNumber: this.state.spanNumber + 1,
+
+  })
+  }
+
+
+  render() {
+    return (
+      <div className={styles.form}>
+        <span>{this.state.spanNumber}</span>
+        <input ref={this.newMessageText} type="text"/>
+        <button onClick={this.addClickHandler}>send</button>
+      </div>
+    )
+  }
 };
 
 export default Form;
