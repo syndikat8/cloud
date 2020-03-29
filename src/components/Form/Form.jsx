1a3b61580e3c9f292e0e5ef3ba67fb6cfa209b13
Dima Matusik <k.syndikat@gmail.com>
2020-03-29 13:29:37 +0300

    add InputIN

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,6 +3,7 @@ import styles from './Form.module.css';
 import Button from "./Button/Button";
 import Input from "./Input/Input";
 import Span from "./Span/Span";
+import InputIN from "./InputIN/InputIN";
 
 
 class Form  extends  React.Component{
@@ -12,16 +13,24 @@ class Form  extends  React.Component{
 
   state = {
     spanNumber: 10,
+    newName: "",
   };
-
 
   addClickHandler = () => {
     let text = this.newMessageText.current.value;
     this.newMessageText.current.value = "";
-    alert("Привет " + text);
+    this.addNewName(text + " ");
   this.setState({
     spanNumber: this.state.spanNumber + 1,
   });
+  };
+
+
+  addNewName = (newName) => {
+    let names = [...this.state.newName, newName];
+    this.setState({
+      newName: names
+    })
   };
 
 
@@ -31,6 +40,7 @@ class Form  extends  React.Component{
         <Span spanNumber={this.state.spanNumber} />
         <Input newMessageText={this.newMessageText}/>
         <Button click={this.addClickHandler}/>
+        <InputIN newName={this.state.newName}/>
       </div>
     )
   }
